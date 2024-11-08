65495e06ff0fe12d0ba7a13acb1fd57e56633c37
Miguel Nazario <95545880+gitMiguel27@users.noreply.github.com>
2024-11-08 14:29:58 -0500

    practice looping through object and array

--- index.js
+++ index.js
@@ -116,3 +116,43 @@ while (c <= 10) {
 //     q++;
 //     c += 0.25;
 // }
+
+
+//Object Practice:
+
+const users = [
+    {
+      id: 1,
+      name: "Leanne Graham",
+      username: "Bret",
+      email: "Sincere@april.biz",
+      address: {},
+      phone: "1-770-736-8031 x56442",
+      website: "hildegard.org",
+      company: {},
+    },
+    {
+      id: 2,
+      name: "Ervin Howell",
+      username: "Antonette",
+      email: "Shanna@melissa.tv",
+      address: {},
+      phone: "010-692-6593 x09125",
+      website: "anastasia.net",
+      company: {},
+    },
+    {
+      id: 3,
+      name: "Clementine Bauch",
+      username: "Samantha",
+      email: "Nathan@yesenia.net",
+      address: {},
+      phone: "1-463-123-4447",
+      website: "ramiro.info",
+      company: {},
+    }
+];
+
+users.forEach((user) => {
+    console.log(user.username);
+});
